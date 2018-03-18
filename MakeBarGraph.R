c54bf9b0c3c072ea4129559d0cb79e4835062fcc
huang632 <xibill.huang@mail.utoronto.ca>
2018-03-18 07:03:50 -0400

    Make Bar

--- MakeBarGraph.R
+++ MakeBarGraph.R
@@ -3,4 +3,4 @@ nums <- read.table("CancerCount.csv",sep = ",", header = TRUE)
 
 mat <- matrix(c(nums$Unfavourable, nums$Favourable), ncol=2) 
 
-barplot(t(mat), horiz = TRUE, xlab = "Number Of Related Genes", col = c( "cornflowerblue","cadetblue3"), legend = c("Unfavourable", "Favourable"))
+barplot(t(mat), horiz = TRUE, names.arg = nums$BodyPart, las=2, main = "Cancer And Related Genes", xlab = "Number Of Related Genes", col = c( "cornflowerblue","cadetblue3"), legend = c("Unfavourable", "Favourable"))
